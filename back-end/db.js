fd03c8f620effb97847dceac4ff46d3523b7c7b4
Serkalem Delelegn <serkalemdelelegn2020@gmail.com>
2024-09-01 13:56:35 +0300

    db

--- back-end/db.js
+++ back-end/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 const connectDb = async () => {
     try {
-        const uri = `mongodb+srv://${process.env.NAME}:${process.env.PASSWORD}@cluster0.l0zfpqw.mongodb.net/newsData?retryWrites=true&w=majority&appName=Cluster0`;
+        const uri = `mongodb+srv://${process.env.MONGO_DB_NAME}:${process.env.MONGO_DB_PASSWORD}@cluster0.l0zfpqw.mongodb.net/newsData?retryWrites=true&w=majority&appName=Cluster0`;
         await mongoose.connect(uri, {
             useNewUrlParser: true,
             useUnifiedTopology: true
